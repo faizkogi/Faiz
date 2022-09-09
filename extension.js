905134a02d47b7e315848f94cb083fd9cd31ab23
faizkogi <107922755+faizkogi@users.noreply.github.com>
2022-09-09 17:08:00 +0100

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,7 @@
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    $('.text').addClass(text-primary);
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
